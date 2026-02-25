a640435db805d9a8e0b57ed58e3250f0e87bc159
shameer125 <alishameer251@gmail.com>
2026-02-25 14:01:04 -0800

    I add h1 to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
       <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quo
+        qui quas voluptatum nam nisi labore, velit vel quod repellat soluta
+        consectetur corporis obcaecati officiis expedita cum commodi atque
+        placeat!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
