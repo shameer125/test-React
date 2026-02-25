44eb91009ad782ae581b08fffaf0dca7cf0ce4e6
shameer125 <alishameer251@gmail.com>
2026-02-25 14:09:12 -0800

    -Navbar details

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero Section</h1>
+      <div className='flex justify-between pt-10 px-10'>
+          <div className='font-bold text-2xl '>
+              <h1>Shameer Ali</h1>
+          </div>
+          <ul className='flex gap-8 text-lg font-medium'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Portfolio</li>
+              <li>Contact</li>
+      </ul>
     </div>
   )
 }
